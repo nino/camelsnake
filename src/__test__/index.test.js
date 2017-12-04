bfc7b38a94aeddcc86112e8400326b3185169ed8
Nino Annighöfer <nino@ninoan.com>
2017-12-04 05:54:03 +0100

    v2.0.0 Remove dependencies and throw exceptions for bad arguments
    Also fixes a bug where snakifying didn't work right
    if a key had more than one underscore.

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,16 +1,35 @@
 /* @flow */
 import test from "ava";
-import { camelize, snakify } from "index";
+import { camelize, snakify } from "../index";
 
 const sampleObjects = [
   { snake: {}, camel: {} },
   { snake: { a: "b" }, camel: { a: "b" } },
-  { snake: { a_key: "a value" }, camel: { aKey: "a value" } }
+  { snake: { a_key: "a value" }, camel: { aKey: "a value" } },
+  {
+    snake: { key_with_underscores: { nested_object: 123 } },
+    camel: { keyWithUnderscores: { nestedObject: 123 } }
+  },
+  {
+    snake: { camel_case_key: { nested_object: 456 } },
+    camel: { camelCaseKey: { nestedObject: 456 } }
+  }
 ];
 
 test("camelize", t => {
   sampleObjects.forEach(object => {
     t.deepEqual(camelize(object.snake), object.camel);
     t.deepEqual(snakify(object.camel), object.snake);
+  });
+});
+
+// TODO threw exception when given an array
+const invalidArguments = ["a string", 123, null, undefined];
+
+test("throw when given non-object arguments", t => {
+  invalidArguments.forEach(arg => {
+    t.throws(() => {
+      camelize(arg);
+    }, TypeError);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,32 +1,45 @@
 /* @flow */
-import isPlainObject from "lodash/isPlainObject";
-import mapKeys from "lodash/mapKeys";
-import mapValues from "lodash/mapValues";
+
+function isPlainObject(maybeObject: mixed): boolean {
+  return (
+    typeof maybeObject === "object" &&
+    Object.keys(Object.getPrototypeOf(maybeObject)).length === 0
+  );
+}
 
 function camelizeString(str: string): string {
   return str.replace(/_(\w)/g, match => match[1].toUpperCase());
 }
 
 function snakifyString(str: string): string {
-  return str.replace(/[A-Z]/, match => "_" + match[0]).toLowerCase();
+  return str.replace(/[A-Z]/g, match => "_" + match[0]).toLowerCase();
+}
+
+function deeplyMapKeys(
+  object: { [string]: mixed },
+  mapper: string => string
+): { [string]: mixed } {
+  const mappedObject = {};
+  Object.keys(object).forEach(key => {
+    const mappedKey = mapper(key);
+    const mappedValue = isPlainObject(object[key])
+      ? deeplyMapKeys(object[key], mapper)
+      : object[key];
+    mappedObject[mappedKey] = mappedValue;
+  });
+  return mappedObject;
 }
 
 export function camelize(object: { [string]: mixed }): { [string]: mixed } {
-  const objectWithMappedKeys = mapKeys(object, (value, key) =>
-    camelizeString(key)
-  );
-  return mapValues(
-    objectWithMappedKeys,
-    subObject => (isPlainObject(subObject) ? camelize(subObject) : subObject)
-  );
+  if (!object || !isPlainObject(object)) {
+    throw new TypeError("`object` is not a plain object");
+  }
+  return deeplyMapKeys(object, camelizeString);
 }
 
 export function snakify(object: { [string]: mixed }): { [string]: mixed } {
-  const objectWithMappedKeys = mapKeys(object, (value, key) =>
-    snakifyString(key)
-  );
-  return mapValues(
-    objectWithMappedKeys,
-    subObject => (isPlainObject(subObject) ? snakify(subObject) : subObject)
-  );
+  if (!object || !isPlainObject(object)) {
+    throw new TypeError("`object` is not a plain object");
+  }
+  return deeplyMapKeys(object, snakifyString);
 }
